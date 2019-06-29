384e1affcdce1b911db000b46c64e529980f720f
hardzal <rizaldoeta98@gmail.com>
2019-06-29 23:30:58 +0700

    Update

--- application/views/users/editprofile.php
+++ application/views/users/editprofile.php
@@ -40,6 +40,14 @@
 							</div>
 						</div>
 
+						<div class="form-group row">
+							<label for="name" class="col-sm-2 col-form-label">Full Name</label>
+							<div class="col-sm-10">
+								<input type="text" class="form-control" id="name" placeholder="Name" name="name" value="<?= $user['name']; ?>" />
+								<?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
+							</div>
+						</div>
+
 						<div class="form-group row justify-content-end">
 							<div class="col-sm-10">
 								<button type="submit" class="btn btn-primary">Edit</button>
